80d0904fb60c6af0d64f4f6eb62e62cae3352d62
Chin-Yeh Chen <kcode8813940518@gmail.com>
2022-02-14 21:59:30 +0800

    add window tk

--- searchFolder_glob.py
+++ searchFolder_glob.py
@@ -1,22 +1,25 @@
 import pathlib
 import os
 import time
+import tkinter as tk
 from win32com.client import Dispatch
+from tkinter import filedialog
+from tkinter import *
 '''
 # os.path.getatime(file) 輸出檔案訪問時間
 # os.path.getctime(file) 輸出檔案的建立時間
 # os.path.getmtime(file) 輸出檔案最近修改時間
 '''
 
-# dirPath = r'F:\\'  # searching path
-dirPath_tmp = input(
-    'input the folder you want to search(ex: F:\):  ')  # searching path
-dirPath = dirPath_tmp + '\\'
+dirPath = r'D:\SEL論文'  # searching path
+# dirPath_tmp = input(
+#     'input the folder you want to search(ex: F:\):  ')  # searching path
+# dirPath = dirPath_tmp + '\\'
 
-# key_word = '**\*.tks'  # searching keyword
+key_word = '**\*2014*.pdf'  # searching keyword
 # searching keyword
-key_word_tmp = input('input the key word(ex: *.pdf, *.xlxs):  ')
-key_word = '**\\' + key_word_tmp
+# key_word_tmp = input('input the key word(ex: *.pdf, *.xlxs):  ')
+# key_word = '**\\' + key_word_tmp
 
 
 files_list = list(pathlib.Path(dirPath).glob(key_word))
@@ -43,3 +46,28 @@ xl.Visible = True  # otherwise excel is hidden
 
 # newest excel does not accept forward slash in path
 wb = xl.Workbooks.Open(path)
+
+
+root = tk.Tk()
+# 視窗標題
+root.title('hello')
+# 寬度 200
+# 高度 250
+# 螢幕位置 X 300
+# 螢幕位置 Y 400
+root.geometry('200x250+300+400')
+# 運行視窗
+
+
+def browse_button():
+    filename = filedialog.askopenfilenames()
+    print("filename")
+    print(filename)
+
+
+    # return filename
+tk.Button(text="Browse", command=browse_button).pack()
+# .grid(row=0, column=3)
+
+# v = StringVar()
+root.mainloop()
